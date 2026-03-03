0bc81e7f106c604a2f6a9ae2e5d6acff31c93e3f
ParthDevInnovator <makwanaunofficialparth@gmail.com>
2026-03-03 23:16:36 +0530

    pro-plane

--- convex/subscription.ts
+++ convex/subscription.ts
@@ -1,11 +1,34 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const getUserSubscriptions=query({
     args:{userId:v.id("users")},
     handler:async(ctx,args)=>{
-        const subscription=await ctx.db.query("subscriptions").filter(q=>q.eq(q.field("userId"),args.userId)).first();
+        const subscription=await ctx.db.query("subscription").filter(q=>q.eq(q.field("userId"),args.userId)).first();
         if(!subscription) return null;
         return subscription;
+    }
+})
+export const upsertSubscription = mutation({
+    args:{
+        userId:v.id("users"),
+        stripeSubscriptionId:v.string(),
+        status:v.string(),
+        planType:v.union(v.literal("month"),v.literal("years")),
+        currentPeriodStart:v.number(),
+        currentPeriodEnd:v.number(),
+        cancelAtPeriodEnd:v.boolean(),
+    },
+    handler:async(ctx,args)=>{
+        const existingSubscription = await ctx.db.query("subscription")
+        .withIndex("by_stripeSubscriptionId",q=>q.eq("stripeSubscriptionId",args.stripeSubscriptionId))
+        .unique();
+        if(existingSubscription){
+            await ctx.db.patch(existingSubscription._id,args);
+        }else{
+            const subscriptionId = await ctx.db.insert("subscription",args);
+            await ctx.db.patch(args.userId, {currentSubscriptionId:subscriptionId});
+        }
+        return {success:true}
     }
 })
--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -4,6 +4,7 @@ import Stripe from "stripe";
 import { api } from "../../../../../convex/_generated/api";
 import { Id } from "../../../../../convex/_generated/dataModel";
 import { NextResponse } from "next/server";
+import { EventType } from "svix/dist/api/eventType";
 
 const convex =  new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 export async function POST(req:Request) {
@@ -21,6 +22,10 @@ export async function POST(req:Request) {
                case"checkout.session.completed":
                await handleCheckoutSessionCompleted(event.data.object as Stripe.Checkout.Session);
                break;
+               case "customer.subscription.created":
+               case "customer.subscription.updated":
+                await handleSubscriptionUpset(event.data.object as Stripe.Subscription ,event.type);
+                break;
                default:
                     console.log(`Unhandled event type:${event.type}`);
                     break;
@@ -48,4 +53,31 @@ async function handleCheckoutSessionCompleted(session:Stripe.Checkout.Session){
           stripePurchaseId:session.id,
 
      })
+}
+
+async function handleSubscriptionUpset(subscription:Stripe.Subscription ,eventType:string) {
+     if(subscription.status!=="active" || !subscription.latest_invoice){
+          console.log(`skipping subscription ${subscription.id}-status:${subscription.status}`);
+          return;
+     }
+     const stripeCustomerId= subscription.customer as string;
+     const user = await convex.query(api.users.getUserByStripeCustomerId,{stripeCustomerId});
+     if(!user){
+          throw new Error(`User not found for stripe Customer Id: ${stripeCustomerId}`);
+     }
+     try {
+          await convex.mutation(api.subscription.upsertSubscription,{
+               userId:user._id,
+               stripeSubscriptionId:subscription.id,
+               status:subscription.status,
+               planType:subscription.items.data[0].plan.interval as "month" || "year",
+               currentPeriodStart:subscription.current_period_start,
+               currentPeriodEnd:subscription.current_period_end,
+               cancelAtPeriodEnd:subscription.cancel_at_period_end,
+
+          })
+          console.log(`Successfully processed ${eventType} for subscription $`)
+     } catch (error) {
+           console.error(`Error processing ${eventType} for subscription ${subscription.id}:`,error);
+     }
 }
